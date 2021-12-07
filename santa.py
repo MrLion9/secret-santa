14012d80d49987396b8f897dbddb3fc39131b62a
MrLion9 <finthehuman9@gmail.com>
2021-12-08 01:43:19 +0300

    Update santa.py

--- santa.py
+++ santa.py
@@ -1,3 +1,4 @@
+import random
 print("""\
                                         
                  \x1b[1;31m(((((((
@@ -17,3 +18,12 @@ print("""\
                   \x1b[1;32m(((((\x1b[0m
 
 """)
+
+homies=['a', 'b', 'c', 'd', 'e']
+copy_homies= homies.copy()
+
+for h in homies:
+	k=random.choice([a for a in copy_homies if a != h])
+	print(h+' to '+k)
+	del copy_homies[copy_homies.index(k)]
+
